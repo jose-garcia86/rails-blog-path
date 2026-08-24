ca75d866b55dde5cb6569f6ac80df2d412248be8
Jose Garcia <jose.garcia.dev86@gmail.com>
2026-08-24 22:10:23 +0200

    Sort published_at with nulls last

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ApplicationRecord
   validates :title, :body, presence: true
   validates :body, presence: true
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc)}
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.now) }
   scope :scheduled, -> { where("published_at > ?", Time.now) }
